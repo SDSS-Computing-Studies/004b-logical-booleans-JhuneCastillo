dbe91c6ad9eb1070b1a982d2daad848c6ffcc9c4
JhuneCastillo <71406970+JhuneCastillo@users.noreply.github.com>
2020-10-02 12:20:45 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -25,10 +25,10 @@ y = math.sqrt(x)
 z = x ** 1/3
 a = str(x)
 
-if int(y + 0.5 ) ** 2 == x and int(x) ** 1/3 == z:  
+if int(y + 0.5 ) ** 2 == x and x ** 1/3 == z:  
     print(a + " is both a perfect square and a perfect cube.")
 elif int(y + 0.5) ** 2 == x:
     print(a + " is only a perfect square.")
-elif (x) ** 1/3 == z:
+elif x ** 1/3 == z:
     print(a + " is only a perfect cube.")
     
